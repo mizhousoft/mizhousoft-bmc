a53cc677037e608b06582b79305d7ecb4bd31350
mizhousoft <lsp_lishuiping@qq.com>
2022-01-15 11:14:48 +0800

    优化前端配置

--- bmc-nodejs/src/container/App.js
+++ bmc-nodejs/src/container/App.js
@@ -4,6 +4,7 @@ import { ConfigProvider, message } from 'antd';
 import zhCN from 'antd/lib/locale-provider/zh_CN';
 import { BASENAME } from '@/config/application';
 import AppRoutes from '../routes';
+import 'antd/dist/antd.css';
 import '@/static/css/antd.css';
 import '@/static/css/public.css';
 import '@/static/css/style.css';
