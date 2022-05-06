6bfca7c0d6ddd8e95f94f81680ac341eb367b5f5
mizhousoft <lsp_lishuiping@qq.com>
2022-05-07 07:56:55 +0800

    优化样式

--- bmc-nodejs/src/views/components/Logo/index.js
+++ bmc-nodejs/src/views/components/Logo/index.js
@@ -2,5 +2,5 @@ import React from 'react';
 import { APPLICATION_NAME } from '@/config/application';
 
 export default function Logo() {
-    return <div className='logo'>{APPLICATION_NAME}</div>;
+    return <div className='mz-logo'>{APPLICATION_NAME}</div>;
 }
