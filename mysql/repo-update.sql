bac55dea7a16f3a27bdcfc1c3ed857393e54f360
mizhousoft <lsp_lishuiping@qq.com>
2022-06-19 16:46:30 +0800

    删除无效升级脚本

--- mysql/repo-update.sql
+++
@@ -1,7 +0,0 @@
-alter table bmc_perm_resource drop index path_UNIQUE;
-
-alter table bmc_perm_resource add `srvId` VARCHAR(32) NULL after id;
-
-CREATE UNIQUE INDEX PERM_RESOURCE_IDX ON bmc_perm_resource (srvId, path);
-
-update BMC_PERM_RESOURCE set srvId = 'BMC' where permName like 'bmc.%';
