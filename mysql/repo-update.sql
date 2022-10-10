819d078e053fe4fa5ed534ede5e04fe901f843a7
mizhousoft <lsp_lishuiping@qq.com>
2022-10-10 15:17:37 +0800

    升级第三方

---
+++ mysql/repo-update.sql
@@ -0,0 +1,5 @@
+alter table bmc_object_identity rename column `value` to `value_x`;
+
+alter table bmc_dict_field rename column `value` to `value_x`;
+
+alter table bmc_dict_json rename column `value` to `value_x`;
