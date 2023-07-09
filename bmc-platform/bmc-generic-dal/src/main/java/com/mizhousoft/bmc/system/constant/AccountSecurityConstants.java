3d84f4048c9ec7d10bcd2c81c3cf8f6f35f8336a
jacklee <lsp_lishuiping@qq.com>
2023-07-10 07:18:47 +0800

    修改session默认超时时间

--- bmc-platform/bmc-generic-dal/src/main/java/com/mizhousoft/bmc/system/constant/AccountSecurityConstants.java
+++ bmc-platform/bmc-generic-dal/src/main/java/com/mizhousoft/bmc/system/constant/AccountSecurityConstants.java
@@ -7,8 +7,8 @@ package com.mizhousoft.bmc.system.constant;
  */
 public interface AccountSecurityConstants
 {
-	// 默认闲置超时时间
-	int DEFAULT_IDLE_TIMEOUT = 10;
+	// 默认闲置超时时间，单位分钟
+	int DEFAULT_IDLE_TIMEOUT = 30;
 
 	// 帐号密码最小长度
 	int ACCOUNT_PASSWD_MIN_LENGTH = 8;
