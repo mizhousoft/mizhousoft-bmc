528a9bc260903ec8679c4529450d3ceeaa58ee4b
jacklee <lsp_lishuiping@qq.com>
2023-11-17 09:45:58 +0800

    解决bug

--- bmc-platform/bmc-account/src/main/java/com/mizhousoft/bmc/authentication/service/impl/AccountAuthcServiceImpl.java
+++ bmc-platform/bmc-account/src/main/java/com/mizhousoft/bmc/authentication/service/impl/AccountAuthcServiceImpl.java
@@ -8,7 +8,7 @@ import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
-import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections4.CollectionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
