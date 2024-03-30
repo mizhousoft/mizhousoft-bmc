f10d4ce268d2bab3ab6704127ecadd68976ae604
jacklee <lsp_lishuiping@qq.com>
2024-03-30 15:04:29 +0800

    修改数据库脚本

--- mysql/repo-schema-h2.sql
+++ mysql/repo-schema-h2.sql
@@ -10,7 +10,7 @@ CREATE TABLE IF NOT EXISTS `bmc_role` (
   `displayNameUS` VARCHAR(32) NOT NULL COMMENT '英文名称',
   `descriptionCN` VARCHAR(255) NULL COMMENT '中文描述',
   `descriptionUS` VARCHAR(255) NULL COMMENT '英文描述',
-  PRIMARY KEY (`id`))
+  PRIMARY KEY (`id`));
 
 
 -- -----------------------------------------------------
@@ -20,7 +20,7 @@ CREATE TABLE IF NOT EXISTS `bmc_account_role` (
   `id` INT NOT NULL AUTO_INCREMENT COMMENT 'ID',
   `accountId` BIGINT NULL COMMENT '帐号ID',
   `roleId` INT NULL COMMENT '角色ID',
-  PRIMARY KEY (`id`))
+  PRIMARY KEY (`id`));
 
 
 -- -----------------------------------------------------
@@ -37,7 +37,7 @@ CREATE TABLE IF NOT EXISTS `bmc_permission` (
   `isAuthz` INT NULL COMMENT '是否要认证',
   `descriptionCN` VARCHAR(255) NULL COMMENT '中文描述',
   `descriptionUS` VARCHAR(255) NULL COMMENT '英文描述',
-  PRIMARY KEY (`id`))
+  PRIMARY KEY (`id`));
 
 
 -- -----------------------------------------------------
@@ -48,7 +48,7 @@ CREATE TABLE IF NOT EXISTS `bmc_role_permission` (
   `srvId` VARCHAR(16) NOT NULL COMMENT '业务服务ID',
   `permName` VARCHAR(64) NOT NULL COMMENT '权限名称',
   `roleName` VARCHAR(32) NOT NULL COMMENT '角色名称',
-  PRIMARY KEY (`id`))
+  PRIMARY KEY (`id`));
 
 
 -- -----------------------------------------------------
@@ -59,7 +59,7 @@ CREATE TABLE IF NOT EXISTS `bmc_history_pwd` (
   `accountId` BIGINT NOT NULL COMMENT '帐号ID',
   `historyPwd` VARCHAR(128) NOT NULL COMMENT '历史密码',
   `modifyTime` DATETIME NOT NULL COMMENT '修改时间',
-  PRIMARY KEY (`id`))
+  PRIMARY KEY (`id`));
 
 
 -- -----------------------------------------------------
@@ -77,7 +77,7 @@ CREATE TABLE IF NOT EXISTS `bmc_security_log` (
   `detail` VARCHAR(2048) NULL COMMENT '详细信息',
   `addInfo` VARCHAR(2048) NULL COMMENT '附加信息',
   `creationTime` DATETIME NULL COMMENT '创建时间',
-  PRIMARY KEY (`id`))
+  PRIMARY KEY (`id`));
 
 
 -- -----------------------------------------------------
@@ -95,7 +95,7 @@ CREATE TABLE IF NOT EXISTS `bmc_operation_log` (
   `detail` VARCHAR(2048) NULL COMMENT '详细信息',
   `addInfo` VARCHAR(2048) NULL COMMENT '附加信息',
   `creationTime` DATETIME NULL COMMENT '创建时间',
-  PRIMARY KEY (`id`))
+  PRIMARY KEY (`id`));
 
 
 -- -----------------------------------------------------
@@ -105,7 +105,7 @@ CREATE TABLE IF NOT EXISTS `bmc_idletimeout` (
   `id` INT NOT NULL AUTO_INCREMENT COMMENT 'ID',
   `accountId` BIGINT NOT NULL COMMENT '帐号ID',
   `timeout` INT NOT NULL COMMENT '超时时间',
-  PRIMARY KEY (`id`))
+  PRIMARY KEY (`id`));
 
 
 -- -----------------------------------------------------
@@ -116,7 +116,7 @@ CREATE TABLE IF NOT EXISTS `bmc_perm_resource` (
   `srvId` VARCHAR(16) NULL COMMENT '业务服务ID',
   `permName` VARCHAR(64) NULL COMMENT '权限名称',
   `path` VARCHAR(128) NULL COMMENT '资源路径',
-  PRIMARY KEY (`id`))
+  PRIMARY KEY (`id`));
 
 
 -- -----------------------------------------------------
@@ -135,7 +135,7 @@ CREATE TABLE IF NOT EXISTS `bmc_account` (
   `lockTime` DATETIME NULL COMMENT '锁定时间',
   `lastAccessTime` DATETIME NULL COMMENT '最后访问时间',
   `lastAccessIpAddr` VARCHAR(20) NULL COMMENT '最后访问IP地址',
-  PRIMARY KEY (`id`))
+  PRIMARY KEY (`id`));
 
 
 -- -----------------------------------------------------
@@ -149,7 +149,7 @@ CREATE TABLE IF NOT EXISTS `bmc_dict_field` (
   `value_x` VARCHAR(256) NULL COMMENT '数值',
   `utime` DATETIME NOT NULL COMMENT '更新时间',
   `ctime` DATETIME NOT NULL COMMENT '创建时间',
-  PRIMARY KEY (`id`))
+  PRIMARY KEY (`id`));
 
 
 -- -----------------------------------------------------
@@ -162,4 +162,4 @@ CREATE TABLE IF NOT EXISTS `bmc_dict_json` (
   `value_x` TEXT NULL COMMENT '数值',
   `utime` DATETIME NOT NULL COMMENT '更新时间',
   `ctime` DATETIME NOT NULL COMMENT '创建时间',
-  PRIMARY KEY (`id`))
+  PRIMARY KEY (`id`));
