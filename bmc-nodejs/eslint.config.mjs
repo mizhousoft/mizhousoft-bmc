96d5b68418f30d5f735a53f0ca856e00ab10bb95
lsp_lishuiping@qq.com <lsp_lishuiping@qq.com>
2026-03-20 16:41:30 +0800

    优化eslint

--- bmc-nodejs/eslint.config.mjs
+++ bmc-nodejs/eslint.config.mjs
@@ -26,10 +26,7 @@ export default antfu(
             'node/prefer-global/process': 'off', // TODO: remove this
             'node/prefer-global/buffer': 'off', // TODO: remove this
             'jsdoc/empty-tags': 'off',
-            'unicorn/prefer-number-properties': 'off',
             'unicorn/prefer-node-protocol': 'off',
-            'unicorn/prefer-includes': 'off', // TODO: remove this
-            'unicorn/no-new-array': 'off',
             'unicorn/prefer-string-starts-ends-with': 'off', // TODO: remove this
             'regexp/no-unused-capturing-group': 'off',
             'regexp/no-misleading-capturing-group': 'off',
@@ -37,23 +34,19 @@ export default antfu(
             'regexp/optimal-quantifier-concatenation': 'off',
             'react-hooks/exhaustive-deps': 'off',
             'react-hooks-extra/no-direct-set-state-in-use-effect': 'off',
-            'react/prefer-destructuring-assignment': 'off', // TODO: remove this
             'react-refresh/only-export-components': 'off', // TODO: remove this
             'react/no-clone-element': 'off',
             'react/no-children-for-each': 'off',
-            'react/no-children-count': 'off',
             'react/no-children-map': 'off',
             'react/no-children-only': 'off',
             'react/no-unstable-default-props': 'off',
             'react/no-create-ref': 'off', // TODO: remove this
             'perfectionist/sort-imports': 'off',
-            'perfectionist/sort-exports': 'off',
-            'perfectionist/sort-named-imports': 'off',
-            'perfectionist/sort-named-exports': 'off',
             'regexp/strict': 'off',
             'unused-imports/no-unused-vars': [2, {
                 args: 'none'
             }],
+            'e18e/prefer-static-regex': 'off',
         },
     },
     compat.configs['flat/recommended'],
